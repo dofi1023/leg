870733ef3505df5255656f35f3dbec19f6bcd616
dofi1023 <dofi1023@naver.com>
2024-11-11 18:09:14 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int test, a, b;
+    scanf("%d", &test);
+    for (int i = 0; i < test; i++) {
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
