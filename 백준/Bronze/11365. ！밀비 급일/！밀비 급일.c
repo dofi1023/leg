cfaaf9f5ae4de980aa39e59508652d348a8e6f45
dofi1023 <dofi1023@naver.com>
2024-11-11 18:10:12 +0900

    [Bronze IV] Title: !밀비 급일, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char str[500];
+
+	while (1) {
+		fgets(str,500, stdin);
+		str[strlen(str) - 1] = '\0';
+
+		if (strcmp(str, "END") == 0)
+			break;
+
+		for (int i = strlen(str) - 1; i >= 0; i--) {
+			printf("%c", str[i]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
