0515933c164c3c1e2f9bca2380c6181a321cb75b
dofi1023 <dofi1023@naver.com>
2024-11-11 17:52:29 +0900

    [Bronze III] Title: 윷놀이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2490. 윷놀이/윷놀이.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+	int yut = 0;
+	
+	for (int j = 0; j < 3; j++)
+	{
+		int sum = 0;
+
+		for (int i = 0; i < 4; i++)
+		{
+			scanf("%d", &yut);
+			if (yut == 0)
+				sum++;
+		}
+
+		if (sum == 0)
+			printf("E\n");
+		else if (sum == 1)
+			printf("A\n");
+		else if (sum == 2)
+			printf("B\n");
+		else if (sum == 3)
+			printf("C\n");
+		else if (sum == 4)
+			printf("D\n");
+		
+	}
+}
