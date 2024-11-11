5edd95acaad3ff7519f4b6a6e1301cd6886c8e35
dofi1023 <dofi1023@naver.com>
2024-11-11 18:16:54 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+	char a = 0;
+	scanf("%c", &a);
+
+
+	if (a == 'N')
+		printf("Naver D2");
+	else if (a == 'n')
+		printf("Naver D2");
+	else
+		printf("Naver Whale");
+	return 0;
+}
