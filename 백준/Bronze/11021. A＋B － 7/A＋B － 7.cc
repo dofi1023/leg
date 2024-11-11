fc91013813470726748b291ae28cf7b40da544c6
dofi1023 <dofi1023@naver.com>
2024-11-11 18:09:44 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int c;
+    scanf("%d", &c);
+
+    int a, b;
+    for (int i = 1; i <= c; i++) {
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d\n", i, a + b);
+
+    }
+    return 0;
+
+}
