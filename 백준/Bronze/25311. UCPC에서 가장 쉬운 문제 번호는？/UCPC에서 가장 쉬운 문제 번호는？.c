2d6b01b28bb9e05a1452cec6e25466e05f45a84e
dofi1023 <dofi1023@naver.com>
2024-11-11 18:17:16 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main() {
+	int a = 0;
+	scanf("%d", &a);
+	if (a == 2018)
+		printf("A");
+	if (a == 2019)
+		printf("A");
+	if (a == 2020)
+		printf("A");
+	if (a == 2021)
+		printf("A");
+	if (a == 2022)
+		printf("A");
+
+}
