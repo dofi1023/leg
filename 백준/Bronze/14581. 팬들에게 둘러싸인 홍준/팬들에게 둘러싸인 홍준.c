b9375bdfe75560b1b1dcc163d1c7ab0a5f85968d
dofi1023 <dofi1023@naver.com>
2024-11-11 18:11:45 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+	char id[21];
+	scanf("%s", id);
+	printf(":fan::fan::fan:\n");
+	printf(":fan::");printf("%s", id);printf("::fan:\n");
+	printf(":fan::fan::fan:\n");
+	return 0;
+}
